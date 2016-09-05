53e55a4928fac5c01147ab3fb2a7c87e1b31eca4
Effi <effoeffi@gmail.com>
2016-09-05 16:24:50 +0300

    Using user home directory in case KUBERNETES_CERT_PATH is not found;

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"k8s.io/kubernetes/pkg/apis/batch"
 	"k8s.io/kubernetes/pkg/client/restclient"
 	client "k8s.io/kubernetes/pkg/client/unversioned"
+	"os/user"
 )
 
 func main() {
@@ -35,11 +36,18 @@ func getJob() *batch.Job {
 func getKubernetesClient() *client.Client {
 	kubeCertPath := os.Getenv("KUBERNETES_CERT_PATH")
 	if kubeCertPath == "" {
-		kubeCertPath = "/home/effi/.minikube"
+		log.Println("KUBERNETES_CERT_PATH is not defined, looking for certificate files in .minikube folder under home directory.")
+		usr, err := user.Current()
+		if err != nil {
+			log.Fatalln("Failed to get home directory.", err)
+		}
+		kubeCertPath = filepath.Join(usr.HomeDir, ".minikube")
+		log.Println(kubeCertPath)
 	}
 	host := os.Getenv("KUBERNETES_HOST")
 	if host == "" {
 		host = "https://192.168.99.100:8443"
+		log.Println("KUBERNETES_HOST is not defined, using", host)
 	}
 	ret, err := client.New(&restclient.Config{
 		Host: host,
